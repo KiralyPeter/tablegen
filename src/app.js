24e204336481e1db5ef9a1e45e99c0ef33258307
Peter Kiraly <peter.kiral@gmail.com>
2023-02-16 18:35:59 +0100

    init

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-// init
+
+const gyumolcsok = [
+    { name: 'szilva', quantity: 35, price: 8 },
+    { name: 'alma', quantity: 45, price: 8.3 },
+    { name: 'körte', quantity: 25, price: 9.5 },
+    { name: 'barack', quantity: 37, price: 12 }
+];
+
+const tbody = document.querySelector("#tbody");
+
+// oldal betöltődésre induljon a generálás...
+
+function generateTbody(){
+    gyumolcsok.forEach( (gyumolcs) => {
+        console.log(gyumolcs);
+    });
+}
+
+generateTbody();
