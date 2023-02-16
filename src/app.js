4bf2328ef6011b0dca9ae4aa6f3c6db605510015
Peter Kiraly <peter.kiral@gmail.com>
2023-02-16 19:57:13 +0100

    buttons added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// const { Button } = require("bootstrap");
 
 const gyumolcsok = [
     { name: 'szilva', quantity: 35, price: 8 },
@@ -27,9 +28,19 @@ function generateTbody(){
         tr.append(tdName);
         tr.append(tdQuantity);
         tr.append(tdPrice);
+        tr.append(generateTdDeleteButton());
 
 
     });
 }
 
 generateTbody();
+
+function generateTdDeleteButton() {
+    let td = document.createElement("td");
+    let button = document.createElement("button");
+    button.textContent = "Törlés";
+    button.classList = "btn btn-warning";
+    td.append(button);
+    return td;
+}
