feb9b44ead3de6e405a9a1059a1f2fdc9f03a6e9
Peter Kiraly <peter.kiral@gmail.com>
2023-03-02 18:51:26 +0100

    módosítási lehetőség folyamatban

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const nameInput = document.querySelector("#name");
 const quantityInput = document.querySelector("#quantity");
 const priveInput = document.querySelector("#price");
 
+const editIdInput = document.querySelector("#editId");
+const editIdName = document.querySelector("#editName");
+const editIdQuantity = document.querySelector("#editQuantity");
+const editIdPrice = document.querySelector("#editPrice");
+
 const gyumolcsok = [
     { id: 1, name: 'szilva', quantity: 35, price: 8 },
     { id: 2, name: 'alma', quantity: 45, price: 8.3 },
@@ -38,6 +43,7 @@ function generateTbody(){
         tr.append(tdQuantity);
         tr.append(tdPrice);
         tr.append(generateTdDeleteButton(gyumolcs.id));
+        tr.append(generateTdEditButton(gyumolcs));
 
 
     });
@@ -67,6 +73,33 @@ function generateTdDeleteButton(id) {
         generateTbody();
 
     });
+    td.append(button);
+    return td;
+}
+
+function generateTdEditButton(fruit) {
+    let td = document.createElement("td");
+    let button = document.createElement("button");
+    button.textContent = "Szerkesztés";
+    button.classList = "btn btn-primary";
+
+    // hogy feldobja a Módosítás ablakot...
+    button.setAttribute("data-bs-toggle", "modal");
+    button.setAttribute("data-bs-target", "#editModal");
+
+    button.addEventListener("click", () => {
+        // console.log("Működik");
+        // console.log(fruit.name);
+        
+        // a megjelenített mezők megkapják az adott gyümölcs adott tulajdonságát (beírja az adatokat..)
+        editIdInput.value = fruit.id;
+        editIdName.value = fruit.name;
+        editIdQuantity.value = fruit.quantity;
+        editIdPrice.value = fruit.price;
+    
+
+    });
+
     td.append(button);
     return td;
 }
