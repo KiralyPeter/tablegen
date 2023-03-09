2dab51896faf41b5089224c5583318b77a293e0a
Peter Kiraly <peter.kiral@gmail.com>
2023-03-09 19:22:00 +0100

    json lekérdezés működik

--- src/app.js
+++ src/app.js
@@ -16,14 +16,34 @@ const editIdPrice = document.querySelector("#editPrice");
 
 const saveEditButton = document.querySelector("#saveEditButton");
 
-const gyumolcsok = [
-    { id: 1, name: 'szilva', quantity: 35, price: 8 },
-    { id: 2, name: 'alma', quantity: 45, price: 8.3 },
-    { id: 3, name: 'körte', quantity: 25, price: 9.5 },
-    { id: 4, name: 'barack', quantity: 37, price: 12 }
-];
+// const gyumolcsok = [
+//     { id: 1, name: 'szilva', quantity: 35, price: 8 },
+//     { id: 2, name: 'alma', quantity: 45, price: 8.3 },
+//     { id: 3, name: 'körte', quantity: 25, price: 9.5 },
+//     { id: 4, name: 'barack', quantity: 37, price: 12 }
+// ];
+
+var gyumolcsok = [];
+const host = "http://localhost:3000/";
+// IPV6-os cím, ha nem működne a fenti: [::1]:3000
+
+function getFruits(){
+    let enpoint = "fruits";
+    let url = host + enpoint;
+
+    fetch(url) // "promise" (?) objektumot ad vissza...
+    // .then( (response) => {console.log(response)});
+    .then( response => response.json())    
+    .then( result => {
+        console.log(result);
+        gyumolcsok = result;
+        generateTbody(); // amikor megtörténik a lekérdezés, akkor generáljuk a táblát
+    });
+    
+};
 
 
+getFruits();
 
 // oldal betöltődésre induljon a generálás...
 
@@ -51,7 +71,7 @@ function generateTbody(){
     });
 }
 
-generateTbody();
+
 
 function generateTdDeleteButton(id) {
     let td = document.createElement("td");
